3d051acb6d3be6984d8e3970d6a340be103c95fa
harshalitalele <harshalitalele@gmail.com>
2019-12-01 02:32:32 -0500

    cors and other solr connection changes

--- Back End/application.py
+++ Back End/application.py
@@ -1,14 +1,32 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, request
+from flask_cors import CORS
+import urllib.request
+import urllib.parse
+import json
 
+app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+solr_url = 'http://3.134.81.216:8983/solr/gettingstarted/select?q=text%3A{}&rows=20&fl=id,text,poi_name'
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/salvador")
-def salvador():
-    return "Hello, Salvador"
+@app.route('/query', methods = ['GET'])
+def query():
+    query = request.args.get('q')
+    query = urllib.parse.quote(query)
+    url = solr_url.format(query)
+    try:
+        print(url)
+        datatest = urllib.request.urlopen(url)
+        docstest = json.load(datatest)['response']['docs']
+    except:
+        print("An exception occurred for Query: " + query)
+        docstest = '[]'
+    docstest = json.dumps(docstest)
+    return docstest
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
