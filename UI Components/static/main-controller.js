9e01d645587d8f0945f45711fb493d1dd9c85cfa
harshalitalele <harshalitalele@gmail.com>
2019-12-01 02:33:02 -0500

    ui changes

--- UI Components/static/main-controller.js
+++ UI Components/static/main-controller.js
@@ -1,5 +1,13 @@
 var app = angular.module('search');
 
-app.controller('mainCtrl', ['$scope', function($scope) {
+app.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.message = "Hello there!";
+	
+	$scope.searchQuery = function() {
+		$http.get('http://192.168.1.55:5000/query?q='+$scope.queryterm).then(function(res) {
+			$scope.message = res;
+		}, function(err) {
+			$scope.message = "this is error";
+		});
+	};
 }]);
